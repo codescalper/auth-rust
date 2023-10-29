fee0b2ffcf0d29e6d6324fdb9ea1e27f89836011
codescalper <msfunbook@gmail.com>
2023-10-29 17:44:16 +0530

    Added some comments for understanding

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use reqwest::blocking::Client;
-use reqwest::Error;
+use reqwest::blocking::Client; //Synchronous request
+use reqwest::Error; //To hanfle request error coming from reqwest
 
 fn main() -> Result<(), Error> {
-    let client = Client::new();
+    let client = Client::new(); //Iniitlize client
     let user = "testuser".to_string();
-    let passwd: Option<String> = None;
+    // let passwd: Option<String> = None; //Setting it to none
+    let passwd: Option<String> = Some("my_actual_password".to_string()); // Setting it to Some
     let res = client
-        .get("http://httpbin.org/get")
+        .get("http://httpbin.org/get") //Maing request
         .basic_auth(user, passwd)
         .send();
 
